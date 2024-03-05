9bc43f6f98205b101d26ca53f7e8914b6ed6de30
annakurtin <anna.kurtin@utexas.edu>
2024-03-05 15:47:08 -0700

    update from laptop

---
+++ R_Scripts/2_Analyses/MarthaZ_occupancy_presentation_unmarked_jags.R
@@ -0,0 +1,164 @@
+
+#############################################
+#OCCUPANCY MODELING IN R
+#############################################
+
+# other resources
+# Occupancy example: https://oliviergimenez.github.io/occupancy-workshop/
+
+##############################################
+
+library(AHMbook)
+library(unmarked)
+library(tidyverse)
+library(jagsUI)
+
+#These notes are based off of Martha's presentation in 2023, which is based off of (This page on her webiste)[https://marthazillig.github.io/2019-12-05-Introduction-to-Occupancy-Models/]
+
+#Simulate some occupancy data and some covariates 
+
+M <- 100 #number of sites 
+J <- 3 #number of visits 
+C <- matrix(NA, nrow = M, ncol = J)
+
+#fill in this data with 0 and 1 with a detection probability of 30%
+# visit 1
+C[,1] <- rbinom(M, 1, 0.3)
+# visit 2
+C[,2] <- rbinom(M, 1, 0.3)
+# visit 3
+C[,3] <- rbinom(M, 1, 0.3)
+#There are no NAs. These would be removed in a usual analysis (downside to unmarked)
+# Baysean methods such as JAGS can handle this
+
+# Simulate some wind
+# note that it is important to scale your variables
+wind <- array(runif(M * J, -1,1), dim = c(M, J)) #making a fake windspeed covariate
+
+# let's make a discrete covariate for habitat quality 
+hab <- c(rep("Good", 33), rep("Med", 33), rep("Poor", 34)) #three habitat types - first 33 are good, next 33 are medium, and last 34 are poor
+
+time <- runif(M,0,4)
+
+temp <- runif(M, 0,4) #fake temperature 
+
+
+
+#Analysis Using Unmarked Package 
+
+#Data needs to be in an unmarkedFrame 
+
+# using unmarkedFrameOccu for an occupancy model 
+umf <- unmarkedFrameOccu(y = C, siteCovs = data.frame(hab = hab, temp = temp), obsCovs = list(wind = wind)) 
+# input the dataframe, the site covariates (things that affect presence), and the observation covariates (things that effect detection probability)
+# automatically converted characters to factors
+
+summary(umf)
+
+#now you can model the data
+#Syntax of unmarked:
+#occu(~obsCov ~siteCov, data) use a ~1 for intercept only 
+
+m1 <- occu(~1 ~1, data = umf) #no covariates for detection, no covariates for occupancy
+
+m1
+# important to check model fit
+## what is the mean detection?
+# outputs are untransformed mean estimate for occupancy at a given site
+# you always want your detection probability to be better than 15%, if it's not more than that you probably can't trust the model
+
+#so let's transform them 
+backTransform(m1, "det") #can only use when there are no covariates 
+# this shows 37%
+
+backTransform(m1, "state")
+#.981- this means 98.1% probability of occupancy at each site
+
+m2 <- occu(~1~hab, data = umf) # no covariates on detection, habitat covariate on state part
+#remember the first level of habitat, "good" habitat (factor = 1) is the intercept, and the intercept for habMed and habPoor is the estimate plus the intercept 
+
+m3 <- occu(~1~hab + temp, data = umf)
+m3
+
+m4 <- occu(~wind ~temp, data = umf)
+# wind covariate for detection part of model, temperature in the state part of the model 
+m4
+
+# She's talking about these outputs like they're a linear regression, but wouldn't this be a logistic regression?
+# go through a put your data into these basic models 
+
+
+
+
+
+#JAGS 
+
+# JAGS needs your data in a list and only takes numeric data
+# JAGS doesn't do anything for you
+
+
+## Step 1: change habitat quality into numeric
+hab2 <- as.factor(hab)#JAGS will not do this for you. JAGS only takes numeric data
+
+#STEP 2: Compile the data into a list 
+data <- list(C=C, wind = wind, hab = as.numeric(hab2), M = nrow(C), J=ncol(C))
+## M and J is number of sites and numer of visits, respecitvely (you have to explicitly specify this)
+# Check out your data: Make sure the different vectors within the matrix are all the same length! 
+data
+
+#Write the model
+## write out the mathematical formula
+# cat concatenates the model into a text files
+# note: since this is baysean, you have to specify your priors
+cat("
+    model{
+    
+     for(k in 1:3){ #set a prior for every level of this factor variable (habitat)
+      beta1[k] ~ dunif(-5,5)
+     }
+    
+    for (i in 1:M){   #loop over sites 
+      z[i] ~ dbern(psi[i]) #state model(occupancy)
+      logit(psi[i]) <- beta0 + beta1[hab[i]]
+      
+      for (j in 1:J) {  #loop over visits 
+        C[i,j] ~ dbin(p[i,j], z[i]) #detection model
+        logit(p[i,j]) <- alpha0 + beta2*wind[i,j]
+      }
+    }
+      
+  #priors (I picked non-informative priors)
+    alpha0 ~ dlogis(0,1)
+    beta0 ~ dnorm(0,1)
+    beta2 ~ dnorm(0,1)
+    
+   
+    
+    }
+    ", file = "\\scripts\\test_model.txt")
+
+params <- c("alpha0", "beta0", "beta1", "beta2") #parameter for the model to monitor 
+## you can also name them wind and habitat but this is the syntax that most people use when talking about JAGS
+
+# Bayesian models need a place to start for latent variables 
+zst <- apply(C, 1, max) #starting values for our latent z variable 
+# take the C data, take the max value and start at place 1
+inits <- function(){list(z = zst, beta1 = rnorm(3))} #starting values 
+
+
+# do your JAGS call
+# you can use jagsUI, where the call is just jags, if you use RJags you use jagsr or something like that
+# give JAGS the data, the model files, the initial values, the parameters to save, the number of chains you want to run, an adaption period (optional), a thinning period (optional), a burnin period (optional - the first x number that you specify it will throw out to find the parameter space), and number of iterations
+fit1_jags <- jags(data = data, model.file = "test_model.txt", inits = inits, parameters.to.save = params, n.chains = 3, n.iter = 5000, n.burnin = 1000)
+
+fit1_jags
+
+# bayesian - does your mean for posterior distribution overlap zero?
+# for model convergence we want all values less than 1.1
+# n effect is the number of draws that R used to determine the value
+
+#another way to check model fit: plot the fitted model
+plot(fit1_jags)
+# we're looking for fuzzy caterpillars 
+
+exp(fit1_jags$mean$alpha0) #important to check that detection probability is greater than 15% 
--- R_Scripts/3_Data_Wrangling_Tidying/Cleaning_Playback_Data/Clean 2023 Playback Data.R
+++ R_Scripts/3_Data_Wrangling_Tidying/Cleaning_Playback_Data/Clean 2023 Playback Data.R
@@ -30,15 +30,20 @@ fwp_mo <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveyMet
 fwp_bs <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveyMetadata_BRS.csv") %>% clean_names()
 umbel <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveyMetadata_UMBEL.csv") %>% clean_names()
 
-# Get the number of playback surveys conducted by UMBEL
+##### DONE Get the number of playback surveys conducted by UMBEL ####
 pb_nwenergy <- umbel %>% filter(!observer %in% c("MIV","AES"))
 nwenergy_surveys <- unique(pb_nwenergy$survey_id)
 length(nwenergy_surveys)
-# get all total surveys
-pb_all <- rbind(fwp_ak,fwp_dj,fwp_mo,fwp_bs,umbel_ak)
 
+
+#### DONE Get the coordinates that are associated with each point ####
+# get all total surveys
+pb_all <- rbind(fwp_ak,fwp_dj,fwp_mo,fwp_bs,umbel)
 # Remove observer AES and MIV (didn't transcribe this data)
 pb_all <- pb_all %>% filter(!observer %in% c("MIV","AES"))
+
+
+# Assign coordinates
 # separate by site
 temp <- pb_all %>% separate(col = survey_id, into = c("site","ID"), sep = "#")
 all_sites <- unique(temp$site) # total of 33 sites, 95 surveys
@@ -70,7 +75,7 @@ pb23_wcoord <- points_all %>% filter(site %in% temp$site)
 # write the playback points to a .csv
 #write.csv(pb23_wcoord, "./Data/Playback_Results/2023/Outputs/2023_PlaybackSiteLocations_All_11-28.csv", row.names = FALSE)
 
-# average the sites
+# average the sites for use in visualization
 points_avg <- pb23_wcoord %>% separate(col = point_id, into = c("site","id"), sep = "-") %>% group_by(site) %>% summarize(lat_avg = mean(lat), long_avg = mean(long))
 # export this for use in ArcGIS
 #write.csv(points_avg, "./Data/Playback_Results/2023/Outputs/2023_PlaybackSiteLocationsAvg_All_11-28.csv", row.names = FALSE)
@@ -85,35 +90,62 @@ points_avg <- pb23_wcoord %>% separate(col = point_id, into = c("site","id"), se
 # Total number of surveys 
 282 + 2 # 284
 
+#### Create output: combine average coordinates for site with detection/nondetection at the site ####
+pb23_pointcoord <- read.csv("./Data/Playback_Results/2023/Outputs/2023_PlaybackSiteLocations_All_11-28.csv")
+pb23_avgsitecoord <- read.csv("./Data/Playback_Results/2023/Outputs/2023_PlaybackSiteLocationsAvg_All_11-28.csv")
+# combine the playback locations with averaged coordinates 
+test <- left_join(pb_all,pb23_wcoord, by = )
+# Combine this with detection data
+# COME BACK TO THIS LATER ________________________________
 
-#### PLAYBACK DATA ####
+
+
+
+
+#### PLAYBACK DATA ##############################################
 # Manually cleaned up csv's to remove spaces 11/21/23
 
-
 #### Clean playback data itself ####
-an_umbel <- read.csv("./Data/Playback_Results/2023/Raw_Data/MMR2023_PlaybackData_clean_akmanuallyedited.csv") %>% clean_names()
+# Other data from UMBEL sites 
+an_umbel <- read.csv("./Data/Playback_Results/2023/Raw_Data/MMR2023_CuckooPlaybackData_v3clean_akmanuallyedited.csv") %>% clean_names()
 # remove some unnecessary columns
 an_umbel <- an_umbel %>% select(-c("entry_order","entry_person","how2")) 
 an_umbel <- an_umbel %>% rename(obs=observer, 
                                 point_id = point, 
                                 site_id = site, 
                                 start_time = time_format)
-# Need to work out the last couple columns, change to visual and call from how1
-ak_umbel <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackPtCtSurveyData_UMBEL.csv") %>% clean_names()
-ak_r6 <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackPtCtSurveyData_FWPR6.csv") %>% clean_names()
-daniel <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveyData_FWP_DANIEL.csv")
-all_23 <- rbind(ak_umbel,ak_r6,daniel, an_umbel)
-# Pull out only NOBI, BBCU or YBCU from my data (UMBEL and FWPR6)
-all_cuckoo <- all_23 %>% filter(species %in% c("BBCU","YBCU","NOBI")) # use this in conjunction with the other data
+# Need to work out the last couple columns, change to visual and call from how1________________________________
+# V is visual C is calling (S is singing)
 
-all_cuckoo <- all_cuckoo %>% mutate(bbcu_detection = ifelse(species == "BBCU", 1, 0), ybcu_detection = ifelse(species == "YBCU", 1, 0))
+# My data from UMBEL sites
+ak_umbel <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackPtCtSurveyData_UMBEL.csv") %>% clean_names() %>% filter(species %in% c("BBCU","YBCU","NOBI"))
+# My data from FWP sites
+ak_r6 <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackPtCtSurveyData_FWPR6.csv") %>% clean_names() %>% filter(species %in% c("BBCU","YBCU","NOBI"))
+# Daniel's data from FWP sites
+dj_r567 <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveyData_FWP_DANIEL.csv")
+# Brandi's Data
+brs_r7 <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveydata_BRS.csv")
+# Region 5 data
+bu_r5 <- read.csv("./Data/Playback_Results/2023/Raw_Data/2023_PlaybackSurveydata_FWPR5.csv")
+
+# Combine all the data into one dataframe
+pb23_all <- rbind(ak_umbel,ak_r6,dj_r567,brs_r7,bu_r5)
+# Create a new column for cuckoo detections
+pb23_all <- pb23_all %>% mutate(bbcu_detection = ifelse(species == "BBCU", 1, 0), ybcu_detection = ifelse(species == "YBCU", 1, 0))
+# Remove the colon from the time
+pb23_all$start_time <- str_replace(pb23_all$start_time,":", "")
+
+
+# Condensing this down
+# We want to condense across each survey ID 
+## Include date, survey_id, start_time (take the minimum), max of bbcu_detection, max of ybcu_detection, average of distance, visual Y if there was a Y in it, call Y if there was a Y in it (maybe transform these to 1's first?), max of cluster
+## Does it matter if some playbacks were stopped/started by a minute or so? I don't think so
+# Pick up here again __________________________________________
 cuckoo_summed <- all_cuckoo %>% group_by(survey_id) %>% summarize(detection_hist_bbcu = max(bbcu_detection),detection_hist_ybcu = max(ybcu_detection)) %>% arrange()
 
 
 # From Daniel's playback data: figure out a way to apply the start time across 1-5 as well as PB1-PB5
 
-# For all data:
-## remove the colon from the times
 
 ### CODE GRAVEYARD #####
 # remove certain lines - already manually cleaned
@@ -132,3 +164,7 @@ cuckoo_summed <- all_cuckoo %>% group_by(survey_id) %>% summarize(detection_hist
 # r6_surveys <- temp %>% filter(site %in% c("ROB","SNO","CUL","CLA"))
 # r7_surveys <- temp %>% filter(!(site %in% c("ROB","SNO","CUL","CLA")))
 # Old: 29 sites, 3 surveys at each site = 87 playback surveys
+
+#condense1_23 <- rbind(ak_umbel,ak_r6,daniel)
+# Pull out only NOBI, BBCU or YBCU from my data (UMBEL and FWPR6)
+#condense1_23 <- condense1_23 %>% filter(species %in% c("BBCU","YBCU","NOBI")) # use this in conjunction with the other data
