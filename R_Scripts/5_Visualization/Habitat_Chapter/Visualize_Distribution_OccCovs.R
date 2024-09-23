1ce3469f084c0d580b7d4f99208f0721e58c4046
annakurtin <anna.kurtin@utexas.edu>
2024-09-23 17:18:51 -0600

    update with newest versions of methods chapter models

--- R_Scripts/5_Visualization/Habitat_Chapter/Visualize_Distribution_OccCovs.R
+++ R_Scripts/5_Visualization/Habitat_Chapter/Visualize_Distribution_OccCovs.R
@@ -4,6 +4,7 @@
 
 # read in data
 us_dat <- read.csv("./Data/Habitat_Model_Covariates/Occupancy_Covariates/All_2023Veg_Covariates_9-4.csv")
+# REMOVE THE SITES THAT WE DIDNT ACTUALLY LOOK AT
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Point Scale Covariates ####
@@ -11,10 +12,17 @@ us_dat <- read.csv("./Data/Habitat_Model_Covariates/Occupancy_Covariates/All_202
 hist(us_dat$dtree_spp_rich, main = "Decid Tree Spp Richness", xlab = "Deciduous Tree Richness (# spp)")
 mean(us_dat$dtree_spp_rich, na.rm = TRUE)
 sd(us_dat$dtree_spp_rich, na.rm = TRUE)
+min(us_dat$dtree_spp_rich, na.rm = TRUE)
+max(us_dat$dtree_spp_rich, na.rm = TRUE)
 
 hist(us_dat$ctree_spp_rich, main = "Conifer Tree Spp Richness", xlab = "Conifer Richness (# spp)")
 mean(us_dat$ctree_spp_rich, na.rm = TRUE)
 sd(us_dat$ctree_spp_rich, na.rm = TRUE)
+min(us_dat$ctree_spp_rich, na.rm = TRUE)
+max(us_dat$ctree_spp_rich, na.rm = TRUE)
+
+floodplain_sites <- nrow(us_dat[us_dat$floodplain_shrub == 1,])
+(floodplain_sites/nrow(us_dat)) * 100
 
 # Broadleaf and floodplain shrub are binary covariates
 
@@ -25,6 +33,8 @@ sd(us_dat$ctree_spp_rich, na.rm = TRUE)
 hist(us_dat$pct_can_landsc, main = "Percent Canopy Landscape", xlab = "Canopy Density (%)")
 mean(us_dat$pct_can_landsc, na.rm = TRUE)
 sd(us_dat$pct_can_landsc, na.rm = TRUE)
+min(us_dat$pct_can_landsc, na.rm = TRUE)
+max(us_dat$pct_can_landsc, na.rm = TRUE)
 
 hist(us_dat$pct_subcan_landsc, main = "Percent Subcanopy Landscape", xlab = "Subcanopy Density (%)")
 mean(us_dat$pct_subcan_landsc, na.rm = TRUE)
@@ -33,10 +43,38 @@ sd(us_dat$pct_subcan_landsc, na.rm = TRUE)
 hist(us_dat$pct_openshrub_landsc, main = "Percent Open Shrub Landscape", xlab = "Open Shrub Density (%)")
 mean(us_dat$pct_openshrub_landsc, na.rm = TRUE)
 sd(us_dat$pct_openshrub_landsc, na.rm = TRUE)
+min(us_dat$pct_openshrub_landsc, na.rm = TRUE)
+max(us_dat$pct_openshrub_landsc, na.rm = TRUE)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Core Scale Covariates ####
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+hist(us_dat$pct_openshrub_core, main = "Percent Shrub/Eary Successional Core")
+mean(us_dat$pct_openshrub_core, na.rm = TRUE)
+sd(us_dat$pct_openshrub_core, na.rm = TRUE)
+min(us_dat$pct_openshrub_core, na.rm = TRUE)
+max(us_dat$pct_openshrub_core, na.rm = TRUE)
+
+hist(us_dat$ht_can_core, main = "Canopy Height Core")
+mean(us_dat$ht_can_core, na.rm = TRUE)
+sd(us_dat$ht_can_core, na.rm = TRUE)
+min(us_dat$ht_can_core, na.rm = TRUE)
+max(us_dat$ht_can_core, na.rm = TRUE)
+
+
+hist(us_dat$ht_subcan_core, main = "Subcanopy Height Core")
+mean(us_dat$ht_subcan_core, na.rm = TRUE)
+sd(us_dat$ht_subcan_core, na.rm = TRUE)
+min(us_dat$ht_subcan_core, na.rm = TRUE)
+max(us_dat$ht_subcan_core, na.rm = TRUE)
+
+hist(us_dat$veg_sd_resid, main = "St Dev All Veg Core Residuals from Regression with Canopy Height")
+mean(us_dat$veg_sd_resid, na.rm = TRUE)
+sd(us_dat$veg_sd_resid, na.rm = TRUE)
+min(us_dat$veg_sd_resid, na.rm = TRUE)
+max(us_dat$veg_sd_resid, na.rm = TRUE)
+
 hist(us_dat$pct_can_core, main = "Percent Canopy Core")
 mean(us_dat$pct_can_core, na.rm = TRUE)
 sd(us_dat$pct_can_core, na.rm = TRUE)
@@ -44,14 +82,6 @@ sd(us_dat$pct_can_core, na.rm = TRUE)
 hist(us_dat$pct_subcan_core, main = "Percent Subcanopy Core")
 mean(us_dat$pct_subcan_core, na.rm = TRUE)
 sd(us_dat$pct_subcan_core, na.rm = TRUE)
-
-hist(us_dat$ht_can_core, main = "Canopy Height Core")
-mean(us_dat$ht_can_core, na.rm = TRUE)
-sd(us_dat$ht_can_core, na.rm = TRUE)
-
-hist(us_dat$ht_subcan_core, main = "Subcanopy Height Core")
-mean(us_dat$ht_subcan_core, na.rm = TRUE)
-sd(us_dat$ht_subcan_core, na.rm = TRUE)
 
 hist(us_dat$sd_allveg_core, main = "St Dev All Veg Core")
 mean(us_dat$sd_allveg_core, na.rm = TRUE)
