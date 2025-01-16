7dad4b3a967c3425e87b0883e7ff24a63baf7a1c
annakurtin <anna.kurtin@utexas.edu>
2025-01-16 10:47:26 -0700

    update with descriptive stats folder and modified scripts for thesis

--- R_Scripts/2_Analyses/Descriptive_Stats/PlaybackData_DescriptiveStats_BasicVisualization.R
+++ R_Scripts/2_Analyses/Descriptive_Stats/PlaybackData_DescriptiveStats_BasicVisualization.R
@@ -1,10 +1,13 @@
 ##### Look at Playback Data #######
 
-# This is a script to look at and visualize how many and where the bbcu and ybcu detections are
+# This is a script to look at descriptive statistics for playback surveys
+
+# visualize how many and where the bbcu and ybcu detections are
+# See how many surveys conducted at each site
 
 # Created 3/25/2024
 
-# Last edited 3/25/2024
+# Last edited 1/16/2025
 
 ##### Setup #######
 packages <- c("stringr","tidyverse","janitor","lubridate","ggplot2")
@@ -25,6 +28,7 @@ pb_23 <- read.csv("./Data/Playback_Results/2023/Outputs/2023_PlaybackSiteLocatio
 pb_23$bbcu_detected <- as.factor(pb_23$bbcu_detected)
 pb_23$ybcu_detected <- as.factor(pb_23$ybcu_detected)
 
+# Look at number of sites
 nrow(pb_21) #35 sites
 sum(pb_21$bbcu_detected) # 4 sites with bbcu
 sum(pb_21$ybcu_detected) # 0 sites with ybcu
@@ -36,6 +40,13 @@ sum(pb_22$ybcu_detected) #1 site with ybcu - 2.5% naive occ
 nrow(pb_23) #33 sites
 sum(pb_23$bbcu_detected) #3 sites with bbcu - 9% naive occ
 sum(pb_23$ybcu_detected) #3 sites with ybcu - 9% naive occ
+
+
+# Look at how many surveys conducted each
+# read in raw data
+surveys_23 <- read.csv("./Data/Playback_Results/2023/Outputs/2023_PBData_FormatforOccMod_8-5-24.csv")
+surveys_23 <- surveys_23 %>% rowwise() %>% mutate(nsurvey = sum(!is.na(c_across(starts_with("X")))))
+table(surveys_23$nsurvey) # 2 with 1 survey, 2 with 2 surveys, other 29 have all three surveys
 
 # Plot BBCU Detections
 pb_21 %>% ggplot(aes(x = long_avg, y = lat_avg, color = bbcu_detected)) +
--- R_Scripts/3_Data_Wrangling_Tidying/Cleaning_Metadata/Look_WhichSites_WhichRivers_ForLiDAR.R
+++ R_Scripts/3_Data_Wrangling_Tidying/Cleaning_Metadata/Look_WhichSites_WhichRivers_ForLiDAR.R
@@ -3,16 +3,44 @@
 # A script to determine from the metadata how many sites are at each study portion for LiDAR data
 
 # Created 6/11/2023
-# last modified 6/11/2023
+# last modified 1/16/2025
 
 #### Setup ####
 packages <- c("tidyverse","janitor", "ggplot2")
 source("./R_Scripts/6_Function_Scripts/Install_Load_Packages.R")
 load_packages(packages)
+source("./R_Scripts/6_Function_Scripts/Create_Site_SamplingColumn_fromPointID.R")
 # Read in cuckoo color palette
-source("./R_Scripts/5_Visualization/Create_CuckooColor_HexCodes.R")
+#source("./R_Scripts/5_Visualization/Create_CuckooColor_HexCodes.R")
 
 #### Read in Data ####
 mdep <- read.csv("./Data/Metadata/Outputs/2023_ARUDeployment_MetadataFull_Cleaned10-24.csv")
 mret <- read.csv("./Data/Metadata/Outputs/2023_ARURetrieval_MetadataFull_Cleaned10-24.csv")
-mret %>% group_by(river) %>% summarize( n_points = n())
+mret <- mret %>% create_site_col()
+# get number of points at each river
+mret %>% group_by(river) %>% summarize(n_points = n())
+# get number of sites at each river
+mret %>% group_by(river) %>% summarize(n_sites = length(unique(site_id)))
+# remove the two we didn't consider from MISO - the site on private and the site we couldn't retrieve
+
+#### Look at playback data as well ####
+#pb23 <- read.csv("./Data/Playback_Results/2023/Outputs/2023_PlaybackMetadata_3-22-24.csv")
+pb23 <- read.csv("./Data/Playback_Results/2023/Outputs/2023_PlaybackSiteLocationsAvg_All_3-22-24.csv") %>% rename(x = long_avg, y = lat_avg)
+
+
+# Split out Upper Missouri River
+# 108.0743650°W 47.6626152°N - easternmost extent of the UMBEL deployments
+# 108.7668825°W 47.4279918°N - southernmost edge of the UMBEL deployments\
+upper_miso <- pb23 %>% filter(x < -108.0743650 & y > 47.4279918)
+
+# Split out Lower Missouri River 
+# Coordinates for southern and westernmost boundary 47.963980, -106.414798
+lower_miso <- pb23 %>% filter(x > -106.414798 & y > 47.963980)
+
+# Split out Yellowstone
+# Coordinates for Nothernmost and westernmost boundary 47.751310, -107.7825650
+yell <- pb23 %>% filter(x > -107.7825650 & y < 47.751310)
+
+# Split out Region 5/Musselshell
+# 107.7825650°W 46.7159112°N 
+mush <- pb23 %>% filter(x < -107.7825650 & y < 46.7159112)
